5cfa202b4142fad66f9eb3acf110903d089a50b8
metatek <gary.seddon@gmail.com>
2022-06-01 17:06:28 +0900

    broken link2

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/community/communication">
+            to="/docs/staff/communication">
             Get Started
           </Link>
         </div>

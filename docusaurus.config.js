9846074b7b212a158ac6a139cdf9eafcc6076c39
metatek <gary.seddon@gmail.com>
2023-04-11 10:45:02 +0900

    added ja locale

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,7 @@ const config = {
   // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['en'],
+    locales: ['en', 'ja'],
   },
 
   presets: [
@@ -102,6 +102,10 @@ const config = {
             docId: 'dts/faq',
             position: 'left',
             label: 'DTS',
+          },
+          {
+            type: 'localeDropdown',
+            position: 'left',
           },
         //  {to: '/blog', label: 'Blog', position: 'left'},
         //  {
